137c6c03c57ff2625502089ca6d3dd951509b38c
Remez19 <rdoneplus@gmail.com>
2022-04-27 17:01:27 +0300

    Adding Models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,28 @@
+from operator import mod
 from django.db import models
-
+from django.core.validators import MinLengthValidator
 # Create your models here.
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    # A charFiled that checks if the text is in format of email address.
+    email_address = models.EmailField()
+
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=20)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    excerpt = models.CharField(max_length=200)
+    image_name = models.CharField(max_length=100)
+    date = models.DateField(auto_now=True)
+    slug = models.SlugField(unique=True)
+    content = models.TextField(validators=[MinLengthValidator(10)])
+    # When we delete author we set this field to null
+    author = models.ForeignKey(
+        Author, on_delete=models.SET_NULL, related_name="posts", null=True)
+    tags = models.ManyToManyField(Tag)
